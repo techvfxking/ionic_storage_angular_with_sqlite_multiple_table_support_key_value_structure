a8674260827f324a4b80dc9a68f3a03a8951c38c
Biplab Sharma <jobs.biplab.sharma@gmail.com>
2023-10-31 23:18:04 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sqlite-with-ionic-storage-test-v1',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
